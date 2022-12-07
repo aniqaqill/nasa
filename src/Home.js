6c7425e622d2b0ca4e956ad0ed5efc2b9c637e37
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2022-12-07 12:10:55 +0800

    first push

---
+++ src/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class home extends React.Component {
+  state = {
+    data: null,
+  };
+
+  componentDidMount() {
+    // Fetch the data from the NASA API
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=jc63aHPramLEhvg8UV3A0aln9kdKTX2ejW76HMFS"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // Update the component's state with the fetched data
+        this.setState({ data });
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    // Check if the data is still loading
+    if (data === null) {
+      return <p>Loading...</p>;
+    }
+
+    // Render the data
+    return (
+      <div>
+        <h1>{data.title}</h1>
+        <p>{data.explanation}</p>
+        <img src={data.url} alt="" />
+
+        <Link to="/search">
+          <button>Search</button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default home;
---
+++ src/Search.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+class Search extends React.Component {
+  state = {
+    images: [],
+    searchTerm: "",
+  };
+
+  handleSearchTermChange = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    // Fetch the search results from the NASA API
+    fetch(
+      `https://images-api.nasa.gov/search?q=${this.state.searchTerm}&media_type=image`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // Extract the image URLs from the search results
+        const images = data.collection.items.map((item) => item.links[0].href);
+
+        // Update the component's state with the search results
+        this.setState({ images });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>NASA Image Search</h1>
+        <form onSubmit={this.handleSearchSubmit}>
+          <label>
+            Search Term:
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.handleSearchTermChange}
+            />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+        <h2>Results</h2>
+        {/* Display the search results */}
+        <ul>
+          {this.state.images.map((image) => (
+            <li key={image}>
+              <img src={image} alt="" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Search;
